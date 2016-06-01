60a0e45317ec84cd3ae8e817b824b683f1976706
lovetoken <lt.lovetoken@gmail.com>
2016-06-01 10:19:58 +0900

    0.21.1 version commit

--- R/midpoint.R
+++ R/midpoint.R
@@ -2,7 +2,7 @@
 #'
 #' 실수 벡터에 대해 midpoint 를 반환합니다.
 #' @param x 가공의 대상이 되는 numeric type 벡터를 입력합니다.
-#' @param ... <code>lead()</code> 함수의 상속 부분입니다.
+#' @param ... \code{lead()} 함수의 상속 부분입니다.
 #' @export
 #' @examples
 #' midpoint(1:10)
