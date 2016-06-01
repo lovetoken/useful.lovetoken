2ae0d37ec9baa05d79a469be0a2b969d2dc81183
lovetoken <lt.lovetoken@gmail.com>
2016-06-01 10:18:56 +0900

    0.21.0 version commit  - `midpoint()` function add

---
+++ R/midpoint.R
@@ -0,0 +1,21 @@
+#' midpoint values
+#'
+#' 실수 벡터에 대해 midpoint 를 반환합니다.
+#' @param x 가공의 대상이 되는 numeric type 벡터를 입력합니다.
+#' @param ... <code>lead()</code> 함수의 상속 부분입니다.
+#' @export
+#' @examples
+#' midpoint(1:10)
+midpoint <- function(x, ...){
+  # pre
+  stopifnot(is.vector(x))
+  stopifnot(require(dplyr))
+
+  # content
+  x_lead <- lead(x)
+  res <- ((x+x_lead)/2) %>% na.omit
+  attributes(res) <- NULL
+
+  # return
+  return(res)
+}
