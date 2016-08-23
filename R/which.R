fbb0836dd35fe2477960a58197ed1fc9d6862cee
lovetoken <lt.lovetoken@gmail.com>
2016-08-23 14:37:57 +0900

    0.4.4 version commit  - add `which_approach()` function

--- R/which.R
+++ R/which.R
@@ -6,9 +6,9 @@
 #' @examples
 #' m <- matrix(sample(c(T,F), 30, T), 5, 6)
 #' which(m)
-#' which.matrix(m)
+#' which_matrix(m)
 
-which.matrix <- function(logical.matrix){
+which_matrix <- function(logical.matrix){
 
   # pre
   stopifnot(is.matrix(logical.matrix))
@@ -20,7 +20,33 @@ which.matrix <- function(logical.matrix){
   qu <- index %/% nr
   re <- index %% nr
 
-  res <- data.frame(row = mapvalues(re, 0, 5), col = (qu+1)-(re==0))
+  res <- data.frame(row = mapvalues(re, 0, 5), col = (qu+1)-(re == 0))
+
+  # return
+  res
+
+}
+
+#' which approach
+#'
+#' which approach index
+#' @param x 찾을 대상이 되는 벡터타입을 입력합니다.
+#' @param k 찾아야 할 최근접점에 대한 numeric 정보를 입력합니다.
+#' @export
+#' @examples
+#' x <- runif(100)
+#' k <- .5
+#' which_approach(x, k)
+
+which_approach <- function(x, k){
+
+  # pre
+  stopifnot(require(dplyr))
+  stopifnot(is.vector(x))
+  stopifnot(is.numeric(k))
+
+  # content
+  res <- abs(x - k) %>% which.min
 
   # return
   res
