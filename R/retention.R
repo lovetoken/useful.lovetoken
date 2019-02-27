d34655c9d9ba0060b30368c9cbf2ccca491e298f
HyoJin Song <lovetoken@nexon.co.kr>
2019-02-27 10:51:04 +0900

    V0.1.62 Commit

--- R/retention.R
+++ R/retention.R
@@ -1,17 +1,19 @@
 #' Retention
 #' @description Retention
+#' @param x input connect log data frame
+#' @param all if FALSE, first date retention
 #' @export
 #' @examples
 #' set.seed(1004)
 #' library(lubridate)
-#' u <- tibble(
+#' connect_log <- tibble(
 #'   id = c(1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 5, 5),
 #'   login_date = sample(seq(ymd_h(2018010100), ymd_h(2018010323), by = "hour"), 20)
 #' ) %>%
 #'   arrange(id, login_date) # must be - first col : index, second col : date
 #' retention(connect_log, by = "days")
 
-retention <- function(x, by = c("days", "weeks", "months")){
+retention <- function(x, by = c("days", "weeks", "months"), all = TRUE){
   x <- x %>%
     dplyr::rename(id = 1, date = 2)
 
@@ -22,13 +24,24 @@ retention <- function(x, by = c("days", "weeks", "months")){
   ) %>%
     dplyr::distinct()
 
-  res <- x %>%
-    dplyr::left_join(x, by = "id") %>%
-    dplyr::rename(first_date = date.x, login_date = date.y) %>%
-    dplyr::mutate(datediff = difftime(login_date, first_date, units = by)) %>%
-    dplyr::filter(datediff >= 0) %>%
-    dplyr::count(first_date, datediff) %>%
-    tidyr::spread(datediff, n)
+  if(all){
+    res <- x %>%
+      dplyr::left_join(x, by = "id") %>%
+      dplyr::rename(first_date = date.x, login_date = date.y) %>%
+      dplyr::mutate(datediff = difftime(login_date, first_date, units = by)) %>%
+      dplyr::filter(datediff >= 0) %>%
+      dplyr::count(first_date, datediff) %>%
+      tidyr::spread(datediff, n)
+  } else {
+    res <- x %>%
+      dplyr::left_join(x, by = "id") %>%
+      dplyr::filter(date.x == min(date.x)) %>%
+      dplyr::rename(first_date = date.x, login_date = date.y) %>%
+      dplyr::mutate(datediff = difftime(login_date, first_date, units = by)) %>%
+      dplyr::filter(datediff >= 0) %>%
+      dplyr::count(first_date, datediff) %>%
+      tidyr::spread(datediff, n)
+  }
 
   return(res)
 }
