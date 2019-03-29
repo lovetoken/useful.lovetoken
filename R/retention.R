d5cf58da2caa6abe0f295c35dc7e57641639ac61
HyoJin Song <lovetoken@nexon.co.kr>
2019-03-29 13:12:19 +0900

    Add feature retention() function

--- R/retention.R
+++ R/retention.R
@@ -11,9 +11,12 @@
 #'   login_date = sample(seq(ymd_h(2018010100), ymd_h(2018010323), by = "hour"), 20)
 #' ) %>%
 #'   arrange(id, login_date) # must be - first col : index, second col : date
+#'
 #' retention(connect_log, by = "days")
+#' retention(connect_log, by = "days", percent_scale = F)
 
-retention <- function(x, by = c("days", "weeks", "months"), all = TRUE){
+retention <- function(x, by = c("days", "weeks", "months"), all = TRUE, percent_scale = TRUE){
+  res <- list()
   x <- x %>%
     dplyr::rename(id = 1, date = 2)
 
@@ -25,7 +28,7 @@ retention <- function(x, by = c("days", "weeks", "months"), all = TRUE){
     dplyr::distinct()
 
   if(all){
-    res <- x %>%
+    res$count <- x %>%
       dplyr::left_join(x, by = "id") %>%
       dplyr::rename(first_date = date.x, login_date = date.y) %>%
       dplyr::mutate(datediff = difftime(login_date, first_date, units = by)) %>%
@@ -33,7 +36,7 @@ retention <- function(x, by = c("days", "weeks", "months"), all = TRUE){
       dplyr::count(first_date, datediff) %>%
       tidyr::spread(datediff, n)
   } else {
-    res <- x %>%
+    res$count <- x %>%
       dplyr::left_join(x, by = "id") %>%
       dplyr::filter(date.x == min(date.x)) %>%
       dplyr::rename(first_date = date.x, login_date = date.y) %>%
@@ -41,6 +44,19 @@ retention <- function(x, by = c("days", "weeks", "months"), all = TRUE){
       dplyr::filter(datediff >= 0) %>%
       dplyr::count(first_date, datediff) %>%
       tidyr::spread(datediff, n)
+  }
+
+  if(percent_scale){
+    res$percent <- x %>%
+      dplyr::left_join(x, by = "id") %>%
+      dplyr::rename(first_date = date.x, login_date = date.y) %>%
+      dplyr::mutate(datediff = difftime(login_date, first_date, units = by)) %>%
+      dplyr::filter(datediff >= 0) %>%
+      dplyr::count(first_date, datediff) %>%
+      dplyr::group_by(first_date) %>%
+      dplyr::mutate(percent = n / max(n)) %>% ungroup %>%
+      dplyr::select(-n) %>%
+      tidyr::spread(datediff, percent)
   }
 
   return(res)
