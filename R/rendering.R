e407c5e823f68fd7ab55e4e1631341a4c3fbb767
lovetoken <lt.lovetoken@gmail.com>
2019-03-29 14:48:12 +0900

    Delete features

--- R/rendering.R
+++ R/rendering.R
@@ -18,17 +18,3 @@ rendering <- function(pattern = NULL, ...){
 		openfd(res)
 	}
 }
-
-#' Code run about .rmd
-#' @description \code{knitr::purl()}에 의하여 \code{.rmd} 스크립트를 R 스크립트로 변환 후 실행합니다. \code{rmarkdown::render()} 함수 보다 저수준 함수로 보는 것이 좋습니다. R 스크립트의 변환을 통해 중간과정을 남기고 추적하기 위한 저수준 함수로 볼 수 있습니다.
-#' @param delete 순수한 R 스크립트 기본적으로 반환되나 필요하지 않을 경우 \code{delete = T} 로 설정하여 지울 수 있습니다.
-#' @export
-#' @examples
-
-source.rmd <- function(file, delete = F, ...) {
-  stopifnot(require(knitr))
-
-  rcode <- purl(file)
-  source(rcode, ...)
-  if(delete) file.remove(rcode)
-}
